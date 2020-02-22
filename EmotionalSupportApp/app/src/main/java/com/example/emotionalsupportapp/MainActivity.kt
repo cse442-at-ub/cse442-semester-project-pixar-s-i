3e595ef293f5ffe5e0bbdae0bf0e563c93bc9329
spittaro17 <43007625+spittaro17@users.noreply.github.com>
2020-02-21 20:38:49 -0500

    Add files via upload

---
+++ EmotionalSupportApp/app/src/main/java/com/example/emotionalsupportapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.emotionalsupportapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
