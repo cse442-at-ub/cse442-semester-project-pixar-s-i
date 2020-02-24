63a941f5ec9b5b1a897945029e0c3659fa4cc33f
Tykfr <wtmills@buffalo.edu>
2020-02-23 19:59:32 -0500

    Added the buttons to the homepage with click Features, waiting for other activities to be created

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/MainActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.emotionalsupportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,14 +14,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Button for High Five
-        Button highFiveBtn = findViewById(R.id.highFiveBtn);
-
     }
 
-
-    public void changePage(View view){
-
+    /*
+     * Changes to the main high five page
+     * should be used to change to the high five page
+     * Called when high five button is clicked
+     */
+    public void goToHighFivePage(View view){
+        Intent intent = new Intent(this,HighFiveActivity.class);
+        startActivity(intent);
+    }
+    /*
+     * Changes to the main hug page
+     * should be used to change to the hug page
+     * Called when hug button is clicked
+     */
+    public void goToHugPage(View view){
+        Intent intent = new Intent(this,HugActivity.class);
+        startActivity(intent);
+    }
+    /*
+     * Changes to the main Motivation page
+     * should be used to change to the Motivation page
+     * Called when Motivation button is clicked
+     */
+    public void goToMotivationPage(View view){
+        Intent intent = new Intent(this,MotivationActivity.class);
+        startActivity(intent);
+    }
+    /*
+     * Changes to the main Talk page
+     * should be used to change to the Talk page
+     * Called when talk button is clicked
+     */
+    public void goToTalkPage(View view){
+        Intent intent = new Intent(this,TalkActivity.class);
+        startActivity(intent);
+    }
+    /*
+     * Changes to the main Connect page
+     * should be used to change to the Connect page
+     * Called when Connect button is clicked
+     */
+    public void goToConnectPage(View view){
+        Intent intent = new Intent(this,ConnectActivity.class);
+        startActivity(intent);
+    }
+    /*
+     * Changes to the main Profile page
+     * should be used to change to the Profile page
+     * Called when Profile button is clicked
+     */
+    public void goToProfilePage(View view){
+        Intent intent = new Intent(this,profile_page.class);
+        startActivity(intent);
     }
 }
