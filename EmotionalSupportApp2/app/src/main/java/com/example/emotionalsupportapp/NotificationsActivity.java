8daf48c4b9aa5c07706c2234677a7944409ce8dd
rex-ix <guangxing@classavo.com>
2020-02-25 17:38:09 -0500

    Update SettingsActivity, HistorysActivity and NotificationsActivity

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationsActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationsActivity.java
@@ -3,6 +3,7 @@ package com.example.emotionalsupportapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+    }
+
+    public void goToHighFiveNotifications(View view) {
+    }
+
+    public void goToHugNotifications(View view) {
+    }
+
+    public void goToMotivationNotifications(View view) {
+    }
+
+    public void goToTalkNotifications(View view) {
+    }
+
+    public void goToConnectNotifications(View view) {
     }
 }
