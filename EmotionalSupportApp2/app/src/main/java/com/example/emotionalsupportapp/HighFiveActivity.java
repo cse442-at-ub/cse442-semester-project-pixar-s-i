e32a7e8f282318ccb690d110e6f509d19619cc70
Tykfr <wtmills@buffalo.edu>
2020-02-26 19:23:46 -0500

    Adding Activites

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveActivity.java
@@ -1,7 +1,8 @@
 package com.example.emotionalsupportapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HighFiveActivity extends AppCompatActivity {
@@ -11,4 +12,17 @@ public class HighFiveActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_five);
     }
+
+    public void returnToMain(View view){
+        Intent returnToMainIntent = new Intent(this, MainActivity.class);
+        startActivity(returnToMainIntent);
+
+    }
+
+    public void beginHighFiveSearch(View view){
+        Intent highFiveSearch = new Intent(this, HighFiveRequestActivity.class);
+        startActivity(highFiveSearch);
+    }
+
+
 }
