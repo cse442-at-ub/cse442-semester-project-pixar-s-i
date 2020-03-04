ea558ae0ec5825dd90b40c49d8841248ac39c837
rex-ix <guangxing@classavo.com>
2020-03-04 17:25:36 -0500

    Add a switch bar and a date dialog for each notifications activity

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationsActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationsActivity.java
@@ -2,6 +2,7 @@ package com.example.emotionalsupportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,17 +15,27 @@ public class NotificationsActivity extends AppCompatActivity {
     }
 
     public void goToHighFiveNotifications(View view) {
+        Intent intent = new Intent(this, HighFiveNotificationsActivity.class);
+        startActivity(intent);
     }
 
     public void goToHugNotifications(View view) {
+        Intent intent = new Intent(this, HugNotificationsActivity.class);
+        startActivity(intent);
     }
 
     public void goToMotivationNotifications(View view) {
+        Intent intent = new Intent(this, MotivationNotificationsActivity.class);
+        startActivity(intent);
     }
 
     public void goToTalkNotifications(View view) {
+        Intent intent = new Intent(this, TalkNotificationsActivity.class);
+        startActivity(intent);
     }
 
     public void goToConnectNotifications(View view) {
+        Intent intent = new Intent(this, ConnectNotificationsActivity.class);
+        startActivity(intent);
     }
 }
