d2ca8796eea3b8e504c9f0e9723f93c215a7f182
Tykfr <wtmills@buffalo.edu>
2020-03-04 13:39:28 -0500

    Adding Notification for High Five - Need to Add Clickable Task #84

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveActivity.java
@@ -1,19 +1,39 @@
 package com.example.emotionalsupportapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import static com.example.emotionalsupportapp.EmotionalSupport.CHANNEL_1_ID;
 
 public class HighFiveActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_five);
+
+        notificationManager = NotificationManagerCompat.from(this);
     }
 
     public void returnToMain(View view){
+        //Notification is made and title and text is set as below for now with a icon, .build to finish the setup
+        Notification notification = new NotificationCompat.Builder(this,CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_notifications_24)
+                .setContentTitle("User request High Five")
+                .setContentText("Click to Confirm HighFive Request")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_EVENT)
+                .build();
+        notificationManager.notify(1,notification);
         Intent returnToMainIntent = new Intent(this, MainActivity.class);
         startActivity(returnToMainIntent);
 
