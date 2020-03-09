448ffff7af950df078b0256ca274f1f3ea751645
rex-ix <guangxing@classavo.com>
2020-03-08 22:03:06 -0400

    Create a dial button for users to call in emergency (task #98)

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
@@ -1,8 +1,12 @@
 package com.example.emotionalsupportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,6 +24,16 @@ public class TalkActivity extends AppCompatActivity {
      */
     public void goToHomePage(View view){
         Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void dialButton_onClick(View view) {
+        String number = "7165800493";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel: " + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
         startActivity(intent);
     }
 }
