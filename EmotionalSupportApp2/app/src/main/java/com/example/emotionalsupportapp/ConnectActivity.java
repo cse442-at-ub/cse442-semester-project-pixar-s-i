3e752be374375aaaf72869b984c3a00766dcf6f8
rex-ix <guangxing@classavo.com>
2020-03-19 22:26:17 -0400

    Design two tabs for users to choose "chats" page and "users" page #118

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/ConnectActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/ConnectActivity.java
@@ -1,22 +1,25 @@
 package com.example.emotionalsupportapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
-    private UserAdapter userAdapter;
-    private ArrayList<String> usernames = new ArrayList<>();
 
 
     @Override
@@ -24,28 +27,48 @@ public class ConnectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
-        usernames.add("user1");
-        usernames.add("user2");
-        usernames.add("user3");
-        usernames.add("user4");
-        usernames.add("user5");
-        usernames.add("user6");
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
 
-        recyclerView = findViewById(R.id.recycler_view);
-        userAdapter = new UserAdapter(usernames, this);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(userAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
-    /*
-     * Changes to the main high five page
-     * should be used to change to the high five page
-     * Called when high five button is clicked
-     */
-    public void goToHomePage(View view){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPagerAdapter(FragmentManager fm){
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position){
+            return titles.get(position);
+        }
     }
 
 
--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/UserAdapter.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/UserAdapter.java
@@ -18,26 +18,23 @@ import java.util.List;
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     private ArrayList<String> username = new ArrayList<>();
-//    private ArrayList<ImageView> profile_image = new ArrayList<>();
     private Context mContext;
 
     public UserAdapter(ArrayList<String> username, Context mContext){
         this.username = username;
-        mContext = mContext;
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_item, parent,false);
-        ViewHolder holder =new ViewHolder(view);
-        return holder;
+        return new UserAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.username.setText(username.get(position));
-        holder.profile_image.setImageResource(R.mipmap.ic_launcher);
 
         holder.parent_layout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,14 +51,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView username;
-        ImageView profile_image;
         RelativeLayout parent_layout;
 
         ViewHolder(View itemView){
             super(itemView);
 
             username = itemView.findViewById(R.id.username);
-            profile_image = itemView.findViewById(R.id.profile_image);
             parent_layout = itemView.findViewById(R.id.parent_layout);
         }
     }
