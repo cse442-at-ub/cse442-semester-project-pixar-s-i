15e742f7fa78d69d0fec5f7a499478b4ee4e7ad0
rex-ix <guangxing@classavo.com>
2020-03-25 20:28:47 -0400

    Display messages in the chat page#104

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/ChatsFragment.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/ChatsFragment.java
@@ -4,10 +4,14 @@ package com.example.emotionalsupportapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 
 /**
@@ -15,17 +19,22 @@ import android.view.ViewGroup;
  */
 public class ChatsFragment extends Fragment {
 
+    private RecyclerView recyclerView;
 
-    public ChatsFragment() {
-        // Required empty public constructor
-    }
-
+    private UserAdapter userAdapter;
+    private ArrayList<String> usernames;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return view;
     }
 
 }
