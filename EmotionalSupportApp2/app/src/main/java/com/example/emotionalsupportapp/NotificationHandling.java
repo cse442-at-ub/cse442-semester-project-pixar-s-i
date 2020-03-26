8bd1eef27fb8c7ff7d15e07f3503a8c29e5af586
Tykfr <36234833+Tykfr@users.noreply.github.com>
2020-03-26 06:36:14 -0400

    Fixing Merge Conflicts

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationHandling.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/NotificationHandling.java
@@ -1,91 +1,19 @@
 package com.example.emotionalsupportapp;
 
-
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+import android.app.Service;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
+import android.os.IBinder;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Random;
-
-import static com.example.emotionalsupportapp.EmotionalSupport.CHANNEL_1_ID;
 
 public class NotificationHandling extends FirebaseMessagingService {
-
     public NotificationHandling() {
     }
 
     @Override
     public void onNewToken(@NonNull String s) {
-
-        //Send token to server with user
         super.onNewToken(s);
     }
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        //Notification is made and title and text is set as below for now with a icon, .build to finish the setup
-        Intent intent;
-        if(remoteMessage.getFrom().equals("highFiver")){
-             intent = new Intent(this,HighFiveVolunteerActivity.class);
-
-        }else if(remoteMessage.getFrom().equals("hugger")){
-            intent = new Intent(this,HugActivity.class);
-        }
-        else{
-            return;
-        }
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        int notificationID = new Random().nextInt(3000);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            setupChannels(notificationManager);
-        }
-
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
-
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
-                R.drawable.ic_notifications_24);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_notifications_24)
-                .setLargeIcon(largeIcon)
-                .setContentTitle(remoteMessage.getData().get("title"))
-                .setContentText(remoteMessage.getData().get("message"))
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent);
-        notificationManager.notify(notificationID, notificationBuilder.build());
-
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void setupChannels(NotificationManager notificationManager){
-        CharSequence adminChannelName = "New notification";
-        String adminChannelDescription = "Device to devie notification";
-
-        NotificationChannel adminChannel;
-        adminChannel = new NotificationChannel(CHANNEL_1_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
-        adminChannel.setDescription(adminChannelDescription);
-        adminChannel.enableLights(true);
-        adminChannel.enableVibration(true);
-        if (notificationManager != null) {
-            notificationManager.createNotificationChannel(adminChannel);
-        }
-    }
-
 }
