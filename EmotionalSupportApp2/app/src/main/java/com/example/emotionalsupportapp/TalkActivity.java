a3d1fc37d792084ee568d986ce10349baa957a73
spittaro17 <Idl2mag4E>
2020-04-01 13:31:17 -0400

    Made the Talk feature call emergency contact as specified on database for #106

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
@@ -9,17 +9,33 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TalkActivity extends AppCompatActivity {
     private static final int REQUEST_CALL = 1;
+    public String phpURLBase = "https://www-student.cse.buffalo.edu/CSE442-542/2020-spring/cse-442e/getInfoProfile.php/?id=" + 1;
+    public RequestQueue reqQueue;
+    String number = "111111111";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk);
+        initializeSql();
     }
     /*
      * Changes to the main high five page
@@ -36,7 +52,7 @@ public class TalkActivity extends AppCompatActivity {
     }
 
     public void makePhoneCall() {
-        String number = "111111111";
+        Log.d("Number Updated:", number);
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel: " + number));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
@@ -54,5 +70,29 @@ public class TalkActivity extends AppCompatActivity {
                 Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public void initializeSql() {
+        reqQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, phpURLBase, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray prof = response.getJSONArray("profiles");
+
+                    number = prof.getJSONObject(0).getString("emergencyContact");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR:", "Error on Volley: " + error.toString());
+            }
+        });
+        reqQueue.add(jsonObjectRequest);
     }
 }
