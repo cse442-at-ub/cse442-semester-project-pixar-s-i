889f571a460a3b99d3069563989fbb5c4381af2a
Tykfr <36234833+Tykfr@users.noreply.github.com>
2020-04-01 19:55:44 -0400

    Adding Location Services/ Fixing Bugs To Hug #112

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveRequestActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HighFiveRequestActivity.java
@@ -14,8 +14,12 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,7 +30,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-
+import org.json.JSONObject;
 
 public class HighFiveRequestActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,46 +41,40 @@ public class HighFiveRequestActivity extends FragmentActivity implements OnMapRe
     private static final int REQUEST_CODE = 101;
     int id;
     Button search;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_five_request);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_high_five_request);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        search  = findViewById(R.id.request_high_five);
+        startLocationService();
+    }
 
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            search = findViewById(R.id.request_high_five);
-            startLocationService();
+    //Method to Start location services
+    void startLocationService() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
         }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    SupportMapFragment supportMapFragment = (SupportMapFragment)
+                            getSupportFragmentManager().findFragmentById(R.id.google_map);
+                    supportMapFragment.getMapAsync(HighFiveRequestActivity.this);
 
-        //Method to Start location services
-        void startLocationService() {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            }
-            Task<Location> task = fusedLocationProviderClient.getLastLocation();
-            task.addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        currentLocation = location;
-                        SupportMapFragment supportMapFragment = (SupportMapFragment)
-                                getSupportFragmentManager().findFragmentById(R.id.google_map);
-                        supportMapFragment.getMapAsync(HighFiveRequestActivity.this);
-
-                    }
                 }
-            });
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            startLocationService();
+            }
+        });
 
-        }
-
-
+    }
     public void returnToMain(View view) {
         Intent returnToMainIntent = new Intent(this, MainActivity.class);
         startActivity(returnToMainIntent);
     }
-
     //    Set Up the Google Maps Location
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -86,17 +84,39 @@ public class HighFiveRequestActivity extends FragmentActivity implements OnMapRe
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         googleMap.addMarker(markerOptions);
+    }
 
+
+    public void sendHFNotification(View v){
+//        reqQueue = Volley.newRequestQueue(getApplicationContext());
+//
+//        String query = "connectToFireBase.php?id=" + 1 + "&topic=/topics/highFiver" + "&lat=" + currentLocation.getLatitude() + "&lon=" + currentLocation.getLongitude()
+//                + "&title=High Five" + "&message=User Request High Five";
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, phpurl + query, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+        search.setText("Locating High Fiver..");
+//        reqQueue.add(jsonObjectRequest);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
+        switch(requestCode){
             case REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     startLocationService();
                 }
                 break;
         }
     }
 }
+
