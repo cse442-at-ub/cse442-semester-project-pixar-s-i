2191a1f3526589073fe360aaf4a4c4fa0378dbdd
William Phillips <wp26@buffalo.edu>
2020-04-01 22:17:25 -0400

    Task #73 Create a confirm button after receiving a hug from a volunteer

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HugConfirmActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/HugConfirmActivity.java
@@ -9,6 +9,6 @@ public class HugConfirmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hug_confirm);
+        setContentView(R.layout.activity_hug_confirm );
     }
 }
