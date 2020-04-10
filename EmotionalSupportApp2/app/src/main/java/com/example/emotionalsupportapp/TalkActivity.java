8cfba86edcd681dee50677f7853940b50a17b741
spittaro17 <Idl2mag4E>
2020-04-10 13:35:10 -0400

    Added Hotline Button to Talk page between call and back buttons #151

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/TalkActivity.java
@@ -66,6 +66,9 @@ public class TalkActivity extends AppCompatActivity {
     public void dialButton_onClick(View view) {
         makePhoneCall();
     }
+    public void hotlineButton_onClick(View view) {
+        makeEmergencyPhoneCall();
+    }
 
     public void makePhoneCall() {
 
@@ -73,7 +76,26 @@ public class TalkActivity extends AppCompatActivity {
         Log.d("Number Updated:", number);
         Intent intent = new Intent(Intent.ACTION_CALL);
         //number
-        intent.setData(Uri.parse("tel: " + "(516)817-7327"));
+        intent.setData(Uri.parse("tel: " + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(TalkActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+            return;
+        }
+        startActivity(intent);
+        String toPrint = phoneStateListener.currentState + ", Prev State = " + phoneStateListener.prevState;
+        Log.d("STATE: ", toPrint);
+
+        Log.d("ROR: ", "EXECUTED UNNESSICARILY");
+
+    }
+
+    public void makeEmergencyPhoneCall() {
+
+
+        Log.d("Number Updated:", number);
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        //number
+        intent.setData(Uri.parse("tel: " + "(516)427-2987"));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(TalkActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
             return;
