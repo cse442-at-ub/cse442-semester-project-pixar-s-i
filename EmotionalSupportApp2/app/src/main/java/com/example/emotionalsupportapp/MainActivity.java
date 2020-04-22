2a69aef80b10265820b7883da5d2d2e563b697ff
rex-ix <guangxing@classavo.com>
2020-04-22 12:55:09 -0400

    Create an indication if unseen messages exist

--- EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/MainActivity.java
+++ EmotionalSupportApp2/app/src/main/java/com/example/emotionalsupportapp/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.emotionalsupportapp.Connect.ConnectActivity;
 import com.example.emotionalsupportapp.Motivation.MotivationActivity;
 import com.example.emotionalsupportapp.Talk.TalkActivity;
@@ -14,9 +21,13 @@ import com.example.emotionalsupportapp.Highfive.HighFiveActivity;
 import com.example.emotionalsupportapp.Hug.HugActivity;
 import com.example.emotionalsupportapp.Member.Profile.profilePage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     String userID;
+    TextView hasUnseenMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
         userID = getIntent().getExtras().getString("EXTRA_USER_ID");
 
+        hasUnseenMessages = findViewById(R.id.has_unseen_messages);
+        checkUnseenMessage(hasUnseenMessages);
     }
 
     /*
@@ -88,7 +101,38 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void checkUnseenMessage(){
+    public void checkUnseenMessage(final TextView hasUnseenMessages){
+        String phpURLBase = "https://www-student.cse.buffalo.edu/CSE442-542/2020-spring/cse-442e/checkUnseenMessages.php/?" +
+                "user_id=" + userID;
+        RequestQueue reqQueue;
+        reqQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, phpURLBase, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String check = response.getString("response");
+                    if (check.equals("exist")){
+                        hasUnseenMessages.setText("!");
+                    }else{
+                        hasUnseenMessages.setText("");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR:", "Error on Volley: " + error.toString());
+            }
+        });
+        reqQueue.add(jsonObjectRequest);
+    }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        checkUnseenMessage(hasUnseenMessages);
     }
 }
